1d84b3b6771b203635d9f6b89b34615094b204c7
Shailesh K <shaileshkhote@gmail.com>
2022-04-17 20:10:19 +0530

    feat: Added Bnb fees on normal token creation

--- contracts/Normal Token/TokenFactory.sol
+++ contracts/Normal Token/TokenFactory.sol
@@ -18,8 +18,24 @@ contract TokenFactory {
         owner=_owner;
     }
 
-    function newToken(string memory _name, string memory _symbol,uint256 _supply, address _minter)
-        public
+     modifier onlyOwner() {
+        require(msg.sender == owner, "Not Owner");
+        
+        _;
+    }
+    function createToken(string memory _name, string memory _symbol,uint256 _supply, address _minter) payable external{
+        require(msg.value == 0.1 ether , "Invalid Amount");
+        deployToken( _name,_symbol, _supply,_minter);
+
+    }
+
+    function withdrawBnb(address payable _sendTo) public onlyOwner(){
+        _sendTo.transfer(address(this).balance);
+
+    }
+
+    function deployToken(string memory _name, string memory _symbol,uint256 _supply, address _minter)
+        internal
     {
         TokenContract token = new TokenContract(_name, _symbol, _supply, _minter);
         tokenName[address(token)]=_name;
