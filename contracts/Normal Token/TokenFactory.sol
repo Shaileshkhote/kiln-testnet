ae5a16291a43fe2c21786ee964246585a43fdd4d
Shailesh K <shaileshkhote@gmail.com>
2022-04-17 20:14:45 +0530

    Refactor: Internal function defined

--- contracts/Normal Token/TokenFactory.sol
+++ contracts/Normal Token/TokenFactory.sol
@@ -25,7 +25,7 @@ contract TokenFactory {
     }
     function createToken(string memory _name, string memory _symbol,uint256 _supply, address _minter) payable external{
         require(msg.value == 0.1 ether , "Invalid Amount");
-        deployToken( _name,_symbol, _supply,_minter);
+        _deployToken( _name,_symbol, _supply,_minter);
 
     }
 
@@ -34,7 +34,7 @@ contract TokenFactory {
 
     }
 
-    function deployToken(string memory _name, string memory _symbol,uint256 _supply, address _minter)
+    function _deployToken(string memory _name, string memory _symbol,uint256 _supply, address _minter)
         internal
     {
         TokenContract token = new TokenContract(_name, _symbol, _supply, _minter);
